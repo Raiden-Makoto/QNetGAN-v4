40665dbb164b06fb479c4eff168c2d07c78461a2
Max <mc.cui@mail.utoronto.ca>
2025-10-17 14:51:59 -0400

    add constants to normalize data

--- utils/adjacency.py
+++ utils/adjacency.py
@@ -1,10 +1,30 @@
 import numpy as np
 import networkx as nx
 import warnings
+
+from typing import Tuple, Optional, Dict
 from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
 from scipy.linalg import fractional_matrix_power
 
-def normalized_adjacency_matrix(smiles: str) -> np.ndarray:
+from utils.constants import (
+    MU_MEAN,
+    MU_STD,
+    ALPHA_MEAN,
+    ALPHA_STD,
+    HOMO_MEAN,
+    HOMO_STD,
+    LUMO_MEAN,
+    LUMO_STD,
+    ZPVE_MEAN,
+    ZPVE_STD,
+    GAP_MEAN,
+    GAP_STD
+)
+
+
+
+def normalized_adjacency_matrix(smiles: str) -> Optional[np.ndarray]:
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         warnings.warn(f"Could not convert {smiles} to a molecule.")
@@ -17,3 +37,18 @@ def normalized_adjacency_matrix(smiles: str) -> np.ndarray:
     deg_inv = fractional_matrix_power(deg, -0.5)
     normalized = deg_inv @ adj @ deg_inv
     return normalized
+
+def adjacency_with_props(smiles: str, props: Dict) -> Tuple[Optional[np.ndarray], Dict]:
+    adj = normalized_adjacency_matrix(smiles)
+    if adj is None:
+        return None, {}
+    
+    # Add molecular properties here
+    props['mu'] = (props['mu'] - MU_MEAN) / MU_STD
+    props['alpha'] = (props['alpha'] - ALPHA_MEAN) / ALPHA_STD
+    props['homo'] = (props['homo'] - HOMO_MEAN) / HOMO_STD
+    props['lumo'] = (props['lumo'] - LUMO_MEAN) / LUMO_STD
+    props['zpve'] = (props['zpve'] - ZPVE_MEAN) / ZPVE_STD
+    props['gap'] = (props['gap'] - GAP_MEAN) / GAP_STD
+    
+    return adj, props
---
+++ utils/constants.py
@@ -0,0 +1,26 @@
+# QM9 dataset normalization constants
+# Calculated from 133,885 molecules in the QM9 dataset
+
+# Dipole moment (mu) - Debye
+MU_MEAN = 2.706037
+MU_STD = 1.530394
+
+# Isotropic polarizability (alpha) - Bohr^3
+ALPHA_MEAN = 75.191296
+ALPHA_STD = 8.187793
+
+# Highest occupied molecular orbital (HOMO) - Hartree
+HOMO_MEAN = -0.239977
+HOMO_STD = 0.022131
+
+# Lowest unoccupied molecular orbital (LUMO) - Hartree
+LUMO_MEAN = 0.011124
+LUMO_STD = 0.046936
+
+# Zero-point vibrational energy (ZPVE) - Hartree
+ZPVE_MEAN = 0.148524
+ZPVE_STD = 0.033274
+
+# HOMO-LUMO gap - Hartree
+GAP_MEAN = 0.251100
+GAP_STD = 0.047519
